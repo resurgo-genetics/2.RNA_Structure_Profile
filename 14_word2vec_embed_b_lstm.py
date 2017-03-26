f659e2fff5aff73655a0867a706e399903a32d22
Freedom_Titan <990295756@qq.com>
2017-03-26 13:33:58 +0800

    fix something for n02

--- 14_word2vec_embed_b_lstm.py
+++ 14_word2vec_embed_b_lstm.py
@@ -32,10 +32,10 @@ seq_index = range(len(Y))
 shuffle(seq_index)
 
 Y = to_categorical(Y)
-x_train ,y_train =pro_input[:SPLIT_point], Y[:SPLIT_point]
-x_valid ,y_valid = pro_input[SPLIT_point:], Y[SPLIT_point:]
+x_train ,y_train =pro_input[seq_index[:SPLIT_point]], Y[seq_index[:SPLIT_point]]
+x_valid ,y_valid = pro_input[seq_index[SPLIT_point:]], Y[seq_index[SPLIT_point:]]
 
-x_train_ehr ,x_valid_ehr =ehr_input[:SPLIT_point], ehr_input[SPLIT_point:]
+x_train_ehr ,x_valid_ehr =ehr_input[seq_index[:SPLIT_point]], ehr_input[seq_index[SPLIT_point:]]
 
 print np.shape(x_train[0]),y_train[0]
 
@@ -54,7 +54,46 @@ from keras import backend as K
 
 # temp
 import numpy as np
+from keras import optimizers as opt
+from keras import backend as K
+from keras.engine.topology import Layer, InputSpec
+from keras import initializations
+# Attention GRU network
+class AttLayer(Layer):
+    def __init__(self, **kwargs):
+        self.init = initializations.get('normal')
+        #self.input_spec = [InputSpec(ndim=3)]
+        super(AttLayer, self).__init__(**kwargs)
 
+    def build(self, input_shape):
+        assert len(input_shape)==3
+        #self.W = self.init((input_shape[-1],1))
+        self.W = self.init((input_shape[-1],))
+        #self.input_spec = [InputSpec(shape=input_shape)]
+        self.trainable_weights = [self.W]
+        super(AttLayer, self).build(input_shape)  # be sure you call this somewhere!
+
+    def call(self, x, mask=None):
+
+
+        M = K.tanh(x)
+        alpha = K.dot(M,self.W)#.dimshuffle(0,2,1)
+
+        ai = K.exp(alpha)
+        weights = ai/K.sum(ai, axis=1).dimshuffle(0,'x')
+        weighted_input = x*weights.dimshuffle(0,1,'x')
+        return K.tanh(weighted_input.sum(axis=1))
+        '''
+        eij = K.tanh(K.dot(x, self.W))
+
+        ai = K.exp(eij)
+        weights = ai/K.sum(ai, axis=1).dimshuffle(0,'x')
+
+        weighted_input = x*weights.dimshuffle(0,1,'x')
+        return weighted_input.sum(axis=1)
+        '''
+    def get_output_shape_for(self, input_shape):
+        return (input_shape[0], input_shape[-1])
 
 EMBEDDING_DIM =100
 MAX_SEQUENCE_LENGTH = 800  
@@ -83,18 +122,20 @@ right.add(ehr_embedding_layer)
 
 model = Sequential()
 model.add(Merge([left, right], mode='concat'))
-model.add(Bidirectional(LSTM(2,return_sequences=True)))
-model.add(Dense(64))
-model.add(Dense(16))
+model.add(Bidirectional(LSTM(100,return_sequences=True)))
+model.add(AttLayer())
 model.add(Dense(2, activation='softmax',activity_regularizer= ActivityRegularizer(l2=0.005)))
-model.compile(loss='mse', optimizer='rmsprop',metrics=['acc'])
+rmsprop = opt.rmsprop(lr=0.0001)
+model.compile(loss='mse', optimizer=rmsprop,metrics=['acc'])
 model.summary()
 
+model.load_weights('./Gen_data/14_w2v_Att-BLSTM_model_1.h5',by_name = True)
 model.fit([x_train, x_train_ehr], y_train, batch_size=128, nb_epoch=100, validation_data=([x_valid, x_valid_ehr], y_valid))
 
 print 'finish!'
 
-
+model.save_weights('./Gen_data/14_w2v_Att-BLSTM_model_2.h5')
+print 'save model finished!'
 
 pro = model.predict_on_batch([x_valid,x_valid_ehr])
 
@@ -104,7 +145,7 @@ from sklearn import metrics
 
 acc = metrics.accuracy_score(y_valid[:,1],y_pred)
 print acc
-a
+
 auc = metrics.roc_auc_score(y_valid[:,1],pro[:,1])
 print auc
 
@@ -113,5 +154,4 @@ print f1_score
 
 
 
-model.save_weights('./Gen_data/14_Att-BLSTM_model.h5')
-print 'save model finished!'
+
